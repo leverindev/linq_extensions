38692be9949f70f2b68523880914ca50582c0d84
Taras Stepanov <t.stepanov@outloud.games>
2024-05-07 09:11:46 +0300

    Outer Join Linq Extensions

---
+++ src/LinqExtensions/JoinableEnumerable.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LinqExtensions
+{
+    public class JoinableEnumerable<T> : IEnumerable<T>
+    {
+        private readonly IEnumerable<T> _collection;
+
+        public JoinableEnumerable(IEnumerable<T> collection, bool isOuter)
+        {
+            _collection = collection;
+            IsOuter = isOuter;
+        }
+
+        public bool IsOuter { get; }
+
+        public IEnumerator<T> GetEnumerator() => _collection.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => _collection.GetEnumerator();
+    }
+}
---
+++ tests/LinqExtensions.Tests/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace LinqExtensions.Tests.Models;
+
+public class Login
+{
+    public int Id { get; init; }
+
+    public string? Name { get; set; }
+}
